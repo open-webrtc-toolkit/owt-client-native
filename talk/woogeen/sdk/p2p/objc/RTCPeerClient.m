b2429110c43a118e5f7dafc0bcf506ed57caa952
Jianjun Zhu <jianjun.zhu@intel.com>
2015-09-30 14:49:44 +0800

    Move iOS SDK to this repo.
    Part of iOS SDK is placed in webrtc-ios-sdk repo. This change moves all iOS SDK
    here.

---
+++ talk/woogeen/sdk/p2p/objc/RTCPeerClient.m
@@ -0,0 +1,218 @@
+//
+//  Copyright (c) 2015 Intel Corporation. All rights reserved.
+//
+
+#import "RTCPeerClient.h"
+#import "RTCP2PErrors.h"
+#import "talk/woogeen/sdk/p2p/objc/RTCP2PPeerConnectionChannel.h"
+
+@interface RTCPeerClient()
+
+-(RTCP2PPeerConnectionChannel*)getPeerConnectionChannel:(NSString*)targetId;
+
+@end
+
+typedef enum{
+  kDisconnected,
+  kConnecting,
+  kConnected
+} SignalingChannelState;
+
+@implementation RTCPeerClient{
+  id<RTCP2PSignalingChannelProtocol> _signalingChannel;
+  SignalingChannelState _peerClientState;
+  NSMutableDictionary *_peerConnectionChannels;
+  NSMutableArray* _observers;
+  NSString* _localId;
+  RTCPeerClientConfiguration* _configuration;
+}
+
+-(instancetype)initWithConfiguration:(RTCPeerClientConfiguration*)configuration signalingChannel:(id<RTCP2PSignalingChannelProtocol>) signalingChannel{
+  self=[super init];
+  _signalingChannel=signalingChannel;
+  [_signalingChannel addObserver:self];
+  _peerConnectionChannels=[[NSMutableDictionary alloc] init];
+  _observers=[[NSMutableArray alloc] init];
+  _configuration=configuration;
+  _peerClientState=kDisconnected;
+  return self;
+}
+
+-(void)addObserver:(id<RTCPeerClientObserver>)observer{
+  [_observers addObject:observer];
+}
+
+-(void)removeObserver:(id<RTCPeerClientObserver>)observer{
+  [_observers removeObject:observer];
+}
+
+-(void)connect:(NSString *)token onSuccess:(void (^)(NSString *))onSuccess onFailure:(void (^)(NSError*))onFailure{
+  _peerClientState=kConnecting;
+  [_signalingChannel connect:token onSuccess:^(NSString *myId){
+    _peerClientState=kConnected;
+    _localId=myId;
+    if(onSuccess){
+      onSuccess(myId);
+    }
+  } onFailure:^(NSError* err){
+    _peerClientState=kDisconnected;
+    if(onFailure){
+      onFailure(err);
+    }
+  }];
+}
+
+-(void)disconnectWithOnSuccess:(void (^)())onSuccess onFailure:(void (^)(NSError *))onFailure{
+  for (id key in _peerConnectionChannels){
+    RTCP2PPeerConnectionChannel* channel = [_peerConnectionChannels objectForKey:key];
+    [channel stopWithOnSuccess:nil onFailure:nil];
+    [_peerConnectionChannels removeObjectForKey:key];
+  }
+  [_signalingChannel disconnectWithOnSuccess: onSuccess onFailure:onFailure];
+}
+
+-(BOOL)checkSignalingChannelOnline:(void(^)(NSError *))failure{
+  if(_peerClientState!=kConnected){
+    if(failure){
+      NSError *err=[[NSError alloc]initWithDomain:RTCErrorDomain code:WoogeenP2PErrorClientInvalidState userInfo:[[NSDictionary alloc]initWithObjectsAndKeys:@"PeerClient haven't connect to a signaling server.", NSLocalizedDescriptionKey, nil]];
+      failure(err);
+    }
+    return NO;
+  }
+  return YES;
+}
+
+-(void)invite:(NSString *)targetId onSuccess:(void (^)())onSuccess onFailure:(void (^)(NSError *))onFailure{
+  if(![self checkSignalingChannelOnline:onFailure])
+    return;
+  RTCP2PPeerConnectionChannel* channel=[self getPeerConnectionChannel:targetId];
+  [channel inviteWithOnSuccess:onSuccess onFailure:onFailure];
+}
+
+
+-(void)publish:(RTCLocalStream *)stream to:(NSString *)targetId onSuccess:(void (^)())onSuccess onFailure:(void (^)(NSError *))onFailure{
+  if(![self checkSignalingChannelOnline:onFailure])
+    return;
+  RTCP2PPeerConnectionChannel* channel=[self getPeerConnectionChannel:targetId];
+  [channel publish:stream onSuccess:onSuccess onFailure:onFailure];
+}
+
+-(void)unpublish:(RTCLocalStream *)stream to:(NSString *)targetId onSuccess:(void (^)())onSuccess onFailure:(void (^)(NSError *))onFailure{
+  if(![self checkSignalingChannelOnline:onFailure])
+    return;
+  RTCP2PPeerConnectionChannel* channel=[self getPeerConnectionChannel:targetId];
+  [channel unpublish:stream onSuccess:onSuccess onFailure:onFailure];
+}
+
+-(RTCP2PPeerConnectionChannel*)getPeerConnectionChannel:(NSString*)targetId{
+  RTCP2PPeerConnectionChannel* channel = [_peerConnectionChannels objectForKey:targetId];
+  if(channel==nil){
+    channel=[[RTCP2PPeerConnectionChannel alloc]initWithConfiguration:_configuration localId:_localId remoteId:targetId signalingSender:self];
+    [channel addObserver:self];
+    [_peerConnectionChannels setObject:channel forKey:targetId];
+  }
+  return channel;
+}
+
+-(void)sendSignalingMessage:(NSString*)data to:(NSString*)targetId onSuccess:(void (^)())onSuccess onFailure:(void (^)(NSError*)) onFailure{
+  [_signalingChannel sendMessage:data to:targetId onSuccess:onSuccess onFailure:onFailure];
+}
+
+-(void)stop:(NSString *)targetId onSuccess:(void (^)())onSuccess onFailure:(void (^)(NSError *))onFailure{
+  if(![self checkSignalingChannelOnline:onFailure])
+    return;
+  RTCP2PPeerConnectionChannel* channel=[self getPeerConnectionChannel:targetId];
+  [channel stopWithOnSuccess:onSuccess onFailure:onFailure];
+  [_peerConnectionChannels removeObjectForKey:targetId];
+}
+
+-(void)deny:(NSString *)targetId onSuccess:(void (^)())onSuccess onFailure:(void (^)(NSError *))onFailure{
+  if(![self checkSignalingChannelOnline:onFailure])
+    return;
+  RTCP2PPeerConnectionChannel* channel=[self getPeerConnectionChannel:targetId];
+  [channel denyWithOnSuccess:onSuccess onFailure:onFailure];
+}
+
+-(void)accept:(NSString *)targetId onSuccess:(void (^)())onSuccess onFailure:(void (^)(NSError *))onFailure{
+  if(![self checkSignalingChannelOnline:onFailure])
+    return;
+  RTCP2PPeerConnectionChannel* channel=[self getPeerConnectionChannel:targetId];
+  [channel acceptWithOnSuccess:onSuccess onFailure:onFailure];
+}
+
+-(void)send:(NSString *)targetId message:(NSString *)message onSuccess:(void (^)())onSuccess onFailure:(void (^)(NSError *))onFailure{
+  if(![self checkSignalingChannelOnline:onFailure])
+    return;
+  RTCP2PPeerConnectionChannel* channel=[self getPeerConnectionChannel:targetId];
+  [channel send:message withOnSuccess:onSuccess onFailure:onFailure];
+}
+
+-(void)onMessage:(NSString *)message from:(NSString *)senderId{
+  RTCP2PPeerConnectionChannel* channel=[self getPeerConnectionChannel:senderId];
+  [channel onIncomingSignalingMessage:message];
+}
+
+-(void)onInvitedFrom:(NSString *)remoteUserId{
+  NSLog(@"On invited from %@", remoteUserId);
+  for (id observer in _observers){
+    [observer onInvited:remoteUserId];
+  }
+}
+
+-(void)onStreamAdded:(RTCRemoteStream *)stream{
+  NSLog(@"PeerClient received stream add.");
+  for (id observer in _observers){
+    [observer onStreamAdded:stream];
+  }
+}
+
+-(void)onStreamRemoved:(RTCRemoteStream *)stream{
+  NSLog(@"PeerClient received stream removed.");
+  for (id observer in _observers){
+    [observer onStreamRemoved:stream];
+  }
+}
+
+-(void)onAcceptedFrom:(NSString *)remoteUserId{
+  NSLog(@"PeerClient received accepted.");
+  for (id observer in _observers){
+    [observer onAccepted:remoteUserId];
+  }
+}
+
+-(void)onDeniedFrom:(NSString *)remoteUserId{
+  NSLog(@"PeerClient received Denied.");
+  for (id observer in _observers){
+    [observer onDenied:remoteUserId];
+  }
+}
+
+-(void)onDataReceivedFrom:(NSString *)remoteUserId withData:(NSString *)data{
+  NSLog(@"Received data from data channel.");
+  for(id observer in _observers){
+    [observer onDataReceived:remoteUserId message:data];
+  }
+}
+
+-(void)onDisconnected{
+  NSLog(@"PeerClient received disconnect.");
+  for (id observer in _observers){
+    [observer onDisconnected];
+  }
+}
+
+-(void)onStoppedFrom:(NSString *)remoteUserId{
+  NSLog(@"PeerClient received chat stopped.");
+  for (id observer in _observers){
+    [observer onChatStopped:remoteUserId];
+  }
+}
+
+-(void)onStartedFrom:(NSString *)remoteUserId{
+  NSLog(@"PeerClient received chat started.");
+  for (id observer in _observers){
+    [observer onChatStarted:remoteUserId];
+  }
+}
+
+@end
---
+++ talk/woogeen/sdk/p2p/objc/public/RTCPeerClientObserver.h
@@ -0,0 +1,87 @@
+/*
+ * Copyright © 2015 Intel Corporation. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+#ifndef p2p_PeerClientObserver_h
+#define p2p_PeerClientObserver_h
+
+#import <Foundation/Foundation.h>
+#import "RTCLocalStream.h"
+#import "RTCRemoteStream.h"
+
+/// Observer for RTCPeerClient
+@protocol RTCPeerClientObserver <NSObject>
+
+/**
+ @brief This function will be invoked when client is disconnected from signaling server.
+ */
+-(void)onServerDisconnected;
+/**
+ @brief This function will be invoked when received a invitation.
+ @param remoteUserId Remote user’s ID
+ */
+-(void)onInvited:(NSString*)remoteUserId;
+/**
+ @brief This function will be invoked when a remote user denied current user's invitation.
+ @param remoteUserId Remote user’s ID
+ */
+-(void)onDenied:(NSString*)remoteUserId;
+/**
+ @brief This function will be invoked when a remote user accepted current user's invitation.
+ @param remoteUserId Remote user’s ID
+ */
+-(void)onAccepted:(NSString*)remoteUserId;
+/**
+ @brief This function will be invoked when a chat is stopped. (This event haven't been implemented yet)
+ @param remoteUserId Remote user’s ID
+ */
+-(void)onChatStopped:(NSString*)remoteUserId;
+/**
+ @brief This function will be invoked when a chat is started. (This event haven't been implemented yet)
+ @param remoteUserId Remote user’s ID
+ */
+-(void)onChatStarted:(NSString*)remoteUserId;
+/**
+ @brief This function will be invoked when received data from a remote user. (This event haven't been implemented yet)
+ @param remoteUserId Remote user’s ID
+ @param message Message received
+ */
+-(void)onDataReceived:(NSString*)remoteUserId message:(NSString*)message;
+/**
+ @brief This function will be invoked when a remote stream is available.
+ @param stream The remote stream added.
+ */
+-(void)onStreamAdded:(RTCRemoteStream*)stream;
+/**
+ @brief This function will be invoked when a remote stream is removed.
+ @param stream The remote stream removed.
+ */
+-(void)onStreamRemoved:(RTCRemoteStream*)stream;
+
+@end
+
+
+#endif
