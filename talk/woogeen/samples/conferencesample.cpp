e9d22f2d042a2f30ba0cd0a94814d480ec0e7fe0
Hua, Chunbo <chunbo.hua@intel.com>
2016-06-14 13:52:39 +0800

    Added the functionality of statistics output every 5 second to native sample
    Change-Id: I52e34b6bc44d8d12b0f9049a95c35953fec3c85c
    Reviewed-on: https://git-ccr-1.devtools.intel.com/gerrit/38020
    Tested-by: webrtctest <webrtctest@intel.com>
    Reviewed-by: Hua, Chunbo <chunbo.hua@intel.com>

--- talk/woogeen/samples/conferencesample.cpp
+++ talk/woogeen/samples/conferencesample.cpp
@@ -3,7 +3,10 @@
 //
 // conferencesample.cpp : Defines the entry point for the console application.
 //
+
 #include <iostream>
+#include <chrono>
+#include <ctime>
 #include <unistd.h>
 
 #include "talk/woogeen/include/asio_token.h"
@@ -41,20 +44,21 @@ int main(int argc, char** argv)
   configuration.ice_servers = ice_servers;
   configuration.media_codec.video_codec = MediaCodec::VideoCodec::VP8;
   //configuration.media_codec.audio_codec = MediaCodec::AudioCodec::ISAC;
+  shared_ptr<ConferenceClient> room(new ConferenceClient(configuration));
+  ConferenceSampleObserver* observer = new ConferenceSampleObserver(room);
+  room->AddObserver(*observer);
+
 
   string scheme("http://");
   string suffix("/createToken");
-  if(argc >= 2){
+  if (argc >= 2) {
     string hosturl(argv[1]);
     scheme.append(hosturl);
-  }else{
+  } else {
     string fixed("10.239.44.59:3001");
     scheme.append(fixed);
   }
   scheme.append(suffix);
-  shared_ptr<ConferenceClient> room(new ConferenceClient(configuration));
-  ConferenceSampleObserver* observer = new ConferenceSampleObserver(room);
-  room->AddObserver(*observer);
 
   string roomId("");
   if(argc == 3){
@@ -108,12 +112,30 @@ int main(int argc, char** argv)
   } else
     cout << "Create token error!" << endl;
 
-  while(1)
+  int interval = 0;
+  while(1) {
     sleep(1);
+    ++interval;
+    if (interval%5 == 0) {
+      room->GetConnectionStats(shared_stream,
+                              [&](std::shared_ptr<ConnectionStats> stats) {
+                                time_t last = chrono::system_clock::to_time_t(stats->time_stamp);
+                                cout << "GetConnectionStats Generated at time of "<< std::ctime(&last) << endl;
+                                cout <<"\n available send bandwidth: " << stats->video_bandwidth_stats.available_send_bandwidth
+                                     <<"\n available receive bandwidth: " << stats->video_bandwidth_stats.available_receive_bandwidth
+                                     <<"\n transmit bitrate: " << stats->video_bandwidth_stats.transmit_bitrate
+                                     <<"\n retransmit_bitrate: " << stats->video_bandwidth_stats.retransmit_bitrate << endl;
+                              },
+                              [=](unique_ptr<ConferenceException>) {
+                                cout << "GetConnectionStats failed" << endl;
+                              });
+    }
+  }
 
   if(observer) {
     delete observer;
     observer = nullptr;
   }
+
   return 0;
 }
