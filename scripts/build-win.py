c694ad99ceb3f81f4eb75877a5b33f6acdebec63
Jianjun Zhu <jianjun.zhu@intel.com>
2016-08-30 14:05:29 +0800

    Integrate tests into build scripts.
    Change-Id: I6ba4977f6abf53c341b073fa7adea8fbc468308c
    Reviewed-on: https://git-ccr-1.devtools.intel.com/gerrit/40393
    Reviewed-by: Zhang, Yanbin <yanbin.zhang@intel.com>
    Reviewed-by: Xian, Yuqiang <yuqiang.xian@intel.com>
    Tested-by: Zhu, Jianjun <jianjun.zhu@intel.com>

--- scripts/build-win.py
+++ scripts/build-win.py
@@ -17,6 +17,11 @@ HOME_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 OUT_PATH = os.path.join(HOME_PATH, 'out')
 OUT_LIB = 'woogeen.lib'
 LIB_BLACK_LIST = ['video_capture']
+# TODO: Disabled tests: common_video_unittests, modules_unittests, rtc_media_unittests, rtc_unittests, video_engine_tests
+PARALLEL_TEST_TARGET_LIST = ['audio_decoder_unittests', 'common_audio_unittests', 'modules_tests', 'peerconnection_unittests',
+    'rtc_pc_unittests', 'system_wrappers_unittests', 'test_support_unittests', 'tools_unittests','voice_engine_unittests',
+    'xmllite_xmpp_unittests']
+NONPARALLEL_TEST_TARGET_LIST = ['webrtc_nonparallel_tests']
 
 def _getlibs(scheme):
   '''Returns an array contains all .lib files' path
@@ -41,12 +46,32 @@ def _mergelibs(scheme):
   command.extend(libs)
   subprocess.call(command, cwd=HOME_PATH)
 
+# Run unit tests on simulator. Return True if all tests are passed.
+def runtest(scheme):
+  test_root_path = os.path.join(OUT_PATH, scheme)
+  for test_target in PARALLEL_TEST_TARGET_LIST:
+    if subprocess.call(['python', 'third_party\gtest-parallel\gtest-parallel', '%s\%s.exe'%(test_root_path, test_target)],
+        cwd=os.path.join(HOME_PATH)):
+      return False
+  for test_target in NONPARALLEL_TEST_TARGET_LIST:
+    if subprocess.call(['%s\%s.exe'%(test_root_path, test_target)],
+        cwd=os.path.join(HOME_PATH)):
+      return False
+  return True
+
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument('--scheme', default='debug',
     help='Schemes for building. Supported value: debug, release')
+  parser.add_argument('--skip_tests', default=False, action='store_true',
+    help='Skip unit tests.')
   opts=parser.parse_args()
+  if not opts.skip_tests:
+    if not runtest(opts.scheme):
+      return 1
   _mergelibs(opts.scheme)
+  print 'Done'
+  return 0
 
 if __name__ == '__main__':
   sys.exit(main())
