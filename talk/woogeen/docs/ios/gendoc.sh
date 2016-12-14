678d38582c824f4a5417e10d30300109d9ad9493
Zhang He <he.zhang@intel.com>
2016-12-14 14:16:30 +0800

    Updated script for generating ios doc.
    Change-Id: Id92c0b4fb1c550e5126b155c9024a2e12d518570
    Reviewed-on: https://git-ccr-1.devtools.intel.com/gerrit/49459
    Tested-by: webrtctest <webrtctest@intel.com>
    Reviewed-by: Zhu, Jianjun <jianjun.zhu@intel.com>

--- talk/woogeen/docs/ios/gendoc.sh
+++ talk/woogeen/docs/ios/gendoc.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+GEN_DOC_PATH=$(cd "$(dirname "$0")"; pwd)
+GEN_HEADER_PATH=$(cd ../../../../scripts; pwd)
+
 #COPY HEADER
-python copyheader.py
+cd $GEN_DOC_PATH
+rm -rf ../../../../out/headers
+python $GEN_HEADER_PATH/generate_objc_headers.py --target_folder ../../../../out/headers
+
 #GENERATE
 rm -rf html
 doxygen doxygen_ios.conf
