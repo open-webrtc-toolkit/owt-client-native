77921d720e72bc125657e51a6f96506d1f0738b2
Qiu Jianlin <jianlin.qiu@intel.com>
2017-04-17 09:36:04 +0800

    Allow rendering from I420 buffer for hardware renderer
    Change-Id: Ibb33eab5d208b92aee991a390d376e7fe092622e
    Reviewed-on: https://git-ccr-1.devtools.intel.com/gerrit/53718
    Tested-by: webrtctest <webrtctest@intel.com>
    Reviewed-by: Zhu, Jianjun <jianjun.zhu@intel.com>
    Reviewed-by: Qiu, Jianlin <jianlin.qiu@intel.com>

--- talk/woogeen/sdk/base/win/videorendererwin.cpp
+++ talk/woogeen/sdk/base/win/videorendererwin.cpp
@@ -1,19 +1,56 @@
 /*
  * Intel License
  */
-#include <atlbase.h>
-#include <codecapi.h>
-#include <combaseapi.h>
-#include <d3d9.h>
-#include <dxva2api.h>
-#include <Windows.h>
+
 #include "talk/woogeen/sdk/base/win/videorendererwin.h"
 #include "talk/woogeen/sdk/base/win/d3dnativeframe.h"
 #include "webrtc/common_video/libyuv/include/webrtc_libyuv.h"
 #include "webrtc/media/base/videocommon.h"
+#include "webrtc/typedefs.h"
 
 namespace woogeen {
 namespace base {
+
+#define D3DFVF_CUSTOMVERTEX (D3DFVF_XYZ | D3DFVF_TEX1)
+struct D3dCustomVertex {
+  float x, y, z;
+  float u, v;
+};
+
+void WebrtcVideoRendererD3D9Impl::Destroy() {
+  m_texture_ = nullptr;
+  m_vertex_buffer_ = nullptr;
+  m_d3d_device_ = nullptr;
+  m_d3d_ = nullptr;
+  inited_for_raw_ = false;
+}
+
+void WebrtcVideoRendererD3D9Impl::Resize(size_t width, size_t height) {
+  width_ = width;
+  height_ = height;
+  IDirect3DTexture9* texture;
+
+  m_d3d_device_->CreateTexture(static_cast<UINT>(width),
+                               static_cast<UINT>(height), 1, 0, D3DFMT_A8R8G8B8,
+                               D3DPOOL_MANAGED, &texture, nullptr);
+  m_texture_ = texture;
+  texture->Release();
+
+  // Vertices for the video frame to be rendered to.
+  static const D3dCustomVertex rect[] = {
+      {-1.0f, -1.0f, 0.0f, 0.0f, 1.0f},
+      {-1.0f, 1.0f, 0.0f, 0.0f, 0.0f},
+      {1.0f, -1.0f, 0.0f, 1.0f, 1.0f},
+      {1.0f, 1.0f, 0.0f, 1.0f, 0.0f},
+  };
+
+  void* buf_data;
+  if (m_vertex_buffer_->Lock(0, 0, &buf_data, 0) != D3D_OK)
+    return;
+
+  memcpy(buf_data, &rect, sizeof(rect));
+  m_vertex_buffer_->Unlock();
+}
 
 void WebrtcVideoRendererD3D9Impl::OnFrame(
     const webrtc::VideoFrame& video_frame) {
@@ -49,13 +86,13 @@ void WebrtcVideoRendererD3D9Impl::OnFrame(
     }
 
     if (surface != nullptr) {
-      hr = pDevice->StretchRect(surface, nullptr, back_buffer, NULL,
+      hr = pDevice->StretchRect(surface, nullptr, back_buffer, nullptr,
                                 D3DTEXF_NONE);
       if (FAILED(hr)) {
         return;
       }
     }
-    pDevice->Present(NULL, NULL, wnd_, NULL);
+    pDevice->Present(nullptr, nullptr, wnd_, nullptr);
 
     dev_manager->UnlockDevice(hDevice, FALSE);
     dev_manager->CloseDeviceHandle(hDevice);
@@ -63,6 +100,66 @@ void WebrtcVideoRendererD3D9Impl::OnFrame(
     // Done with the native handle.
     delete nativeHandle;
     nativeHandle = nullptr;
+  } else {  // I420 frame passed.
+    if (!inited_for_raw_) {
+      m_d3d_ = Direct3DCreate9(D3D_SDK_VERSION);
+      if (!m_d3d_)
+        return;
+
+      D3DPRESENT_PARAMETERS d3d_params = {};
+
+      d3d_params.Windowed = true;
+      d3d_params.SwapEffect = D3DSWAPEFFECT_COPY;
+
+      IDirect3DDevice9* d3d_device;
+      if (m_d3d_->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, wnd_,
+                               D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3d_params,
+                               &d3d_device) != D3D_OK) {
+        Destroy();
+        return;
+      }
+      m_d3d_device_ = d3d_device;
+      d3d_device->Release();
+
+      IDirect3DVertexBuffer9* vertex_buffer;
+      const int kRectVertices = 4;
+      if (m_d3d_device_->CreateVertexBuffer(
+              kRectVertices * sizeof(D3dCustomVertex), 0, D3DFVF_CUSTOMVERTEX,
+              D3DPOOL_MANAGED, &vertex_buffer, nullptr) != D3D_OK) {
+        Destroy();
+        return;
+      }
+      m_vertex_buffer_ = vertex_buffer;
+      vertex_buffer->Release();
+
+      m_d3d_device_->SetRenderState(D3DRS_CULLMODE, D3DCULL_NONE);
+      m_d3d_device_->SetRenderState(D3DRS_LIGHTING, FALSE);
+      Resize(video_frame.width(), video_frame.height());
+      inited_for_raw_ = true;
+    } else {
+      if (static_cast<size_t>(video_frame.width()) != width_ ||
+          static_cast<size_t>(video_frame.height()) != height_) {
+        Resize(static_cast<size_t>(video_frame.width()),
+               static_cast<size_t>(video_frame.height()));
+      }
+      D3DLOCKED_RECT lock_rect;
+      if (m_texture_->LockRect(0, &lock_rect, nullptr, 0) != D3D_OK)
+        return;
+
+      ConvertFromI420(video_frame, webrtc::kARGB, 0,
+                      static_cast<uint8_t*>(lock_rect.pBits));
+      m_texture_->UnlockRect(0);
+
+      m_d3d_device_->BeginScene();
+      m_d3d_device_->SetFVF(D3DFVF_CUSTOMVERTEX);
+      m_d3d_device_->SetStreamSource(0, m_vertex_buffer_, 0,
+                                     sizeof(D3dCustomVertex));
+      m_d3d_device_->SetTexture(0, m_texture_);
+      m_d3d_device_->DrawPrimitive(D3DPT_TRIANGLESTRIP, 0, 2);
+      m_d3d_device_->EndScene();
+
+      m_d3d_device_->Present(nullptr, nullptr, wnd_, nullptr);
+    }
   }
 
   return;
