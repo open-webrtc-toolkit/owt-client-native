133a31cc2f9dc96dcd2edbf8a62f170a1556478b
Zhang He <he.zhang@intel.com>
2017-06-14 10:35:21 +0800

    Fixed compiling issue after rebase to M60.
    1. Moved woogeen android folder to webrtc/sdk
    2. Removed distjar patch
    
    Change-Id: Ie2a9a8fef2beb5370347b5f77a3905d2094118ad
    Reviewed-on: https://git-ccr-1.devtools.intel.com/gerrit/56089
    Reviewed-by: Qiu, Jianlin <jianlin.qiu@intel.com>
    Tested-by: Qiu, Jianlin <jianlin.qiu@intel.com>

--- scripts/prepare_dev.py
+++ scripts/prepare_dev.py
@@ -13,7 +13,6 @@ import subprocess
 HOME_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 PATCH_PATH = os.path.join(HOME_PATH, 'talk', 'woogeen', 'patches')
 TESTING_PATH = os.path.join(HOME_PATH, 'testing')
-BUILD_PATH = os.path.join(HOME_PATH, 'build')
 
 def _replace_gn_files():
   shutil.copyfile(os.path.join(PATCH_PATH, 'libsrtp.gn'), os.path.join(HOME_PATH, 'third_party','libsrtp','BUILD.gn'))
@@ -24,8 +23,6 @@ def _patch():
     subprocess.call(['git', 'am', '--skip'], cwd=TESTING_PATH)
   if (subprocess.call(['git', 'am', os.path.join(PATCH_PATH, 'iossim2.patch')], cwd=TESTING_PATH)) != 0:
     subprocess.call(['git', 'am', '--skip'], cwd=TESTING_PATH)
-  if (subprocess.call(['git', 'am', os.path.join(PATCH_PATH, 'androiddistjar.patch')], cwd=BUILD_PATH)) != 0:
-    subprocess.call(['git', 'am', '--skip'], cwd=BUILD_PATH)
 
 def main(argv):
   _replace_gn_files()
