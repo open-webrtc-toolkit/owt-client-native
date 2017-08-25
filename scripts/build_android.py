1970846bba4bea33b514174cb8e798be3c69c50f
Zhang He <he.zhang@intel.com>
2017-08-25 15:01:31 +0800

    Add script for building and publishing webrtc libs for android.
    Change-Id: Icf423c8a4e17d42f06ad67f33cfb18c2f72a52c9
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/88870
    Reviewed-by: Zhang, He <he.zhang@intel.com>
    Tested-by: Zhang, He <he.zhang@intel.com>

---
+++ scripts/build_android.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python
+# Copyright (c) 2017 Intel Corporation. All Rights Reserved.
+
+'''Script to build WebRTC libs for Android.
+'''
+import os
+import subprocess
+import argparse
+import shutil
+
+HOME_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+OUTPUT_PATH = os.path.join(HOME_PATH, 'out')
+
+def get_location(arch, debug):
+    return os.path.join(OUTPUT_PATH, ('debug' if debug else 'release') + arch)
+
+def gn_gen(arch, debug):
+    print '\n> generating args for', arch, ('debug' if debug else 'release')
+    output_location = get_location(arch, debug)
+    gn_args = '--args=target_os=\"android\" is_component_build=false rtc_include_tests=false '\
+              'target_cpu=\"' + arch +\
+              '\" is_debug=' + ('true' if debug else 'false')
+    cmd = ['gn', 'gen', output_location, gn_args]
+    if subprocess.call(cmd) :
+        sys.exit();
+
+def ninja_build(arch, debug):
+    print '\n> building libjingle_woogeen_so for', arch, ('debug' if debug else 'release')
+    output_location = get_location(arch, debug)
+    cmd = ['ninja', '-C', output_location, 'libjingle_woogeen_so']
+    if subprocess.call(cmd) :
+        sys.exit();
+
+    print '\n> building libwebrtc for', arch, ('debug' if debug else 'release')
+    cmd = ['ninja', '-C', output_location, 'libjingle_woogeen_java']
+    if subprocess.call(cmd) :
+        sys.exit();
+
+def dist(arch, debug):
+    print '\n> copying libs to distribution location'
+    output_location = get_location(arch, debug)
+    jar_location = os.path.join(output_location, 'lib.java/libjingle_woogeen_java.jar')
+    so_location = os.path.join(output_location, 'lib.unstripped/libjingle_woogeen_so.so') if debug\
+                  else os.path.join(output_location, 'libjingle_woogeen_so.so')
+    to_location = os.path.join(OUTPUT_PATH, 'dist', ('debug' if debug else 'release'))
+
+    #copy .jar file
+    cmd = ['cp', jar_location, to_location + '/libjingle_peerconnection.jar']
+    subprocess.call(cmd)
+
+    #change the folder names to keep consistent with android jni folder.
+    if arch == 'arm':
+        arch = 'armeabi-v7a'
+    if arch == 'arm64':
+        arch = 'arm64-v8a'
+
+    if not os.path.exists(os.path.join(OUTPUT_PATH, 'dist')):
+        os.makedirs(os.path.join(OUTPUT_PATH, 'dist'))
+    if not os.path.exists(os.path.join(to_location, arch)):
+        os.makedirs(os.path.join(to_location, arch))
+
+    #copy .so file
+    cmd = ['cp', so_location, os.path.join(to_location, arch, 'libjingle_peerconnection_so.so')]
+    subprocess.call(cmd)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    #build libs for all platforms by default
+    parser.add_argument('--arch', default = 'arm,arm64,x86', dest = 'target_arch',
+        choices = ['arm', 'arm64', 'x86'],
+        help = 'Target architecture(s) to be built, all arch by default.')
+
+    #build release version by default
+    parser.add_argument('-d', '--debug', default = False, dest = 'debug',
+        action = 'store_true',
+        help = 'Indicates to build debug version libs, release version by default.')
+
+    parser.add_argument('--dist', default = os.path.join(HOME_PATH, 'out'),
+        dest = 'output_path',
+        help = 'Distribution package location, /out by default.')
+    options = parser.parse_args()
+
+    print '\n> args:', options
+    OUTPUT_PATH = options.output_path
+    dist_path = os.path.join(OUTPUT_PATH, 'dist', ('debug' if options.debug else 'release'))
+
+    #clean dist location
+    if os.path.exists(dist_path):
+        shutil.rmtree(dist_path)
+    os.makedirs(dist_path)
+
+    for arch in options.target_arch.split(','):
+        gn_gen(arch, options.debug)
+        ninja_build(arch, options.debug)
+        dist(arch, options.debug)
+
+    print '\n> Done building. Find me here:', OUTPUT_PATH + '/dist'\
+          + ('/debug' if options.debug else '/release')
---
+++ scripts/publish_maven.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python
+
+import os
+import sys
+import shutil
+import subprocess
+import argparse
+import datetime
+import commands
+
+# Maven related variables
+IP = '10.239.61.107:60000'
+URL = 'http://' + IP + '/nexus/service/local/artifact/maven/content'
+REPOSITORY = 'thirdparty'
+USERNAME = 'admin'
+PASSWORD = 'admin123'
+CLASSIFIER = ''
+PACKAGING = 'jar'
+GROUP_ID = 'woogeen'
+
+# calendar
+date = datetime.datetime.now()
+year = str(date.year)
+month = str(date.month)
+day = str(date.day)
+hour = str(date.hour)
+DEFAULT_VERSION = year[2:4] + '.' +\
+                  (month if len(month) < 2 else ('0' + month)) + '.' +\
+                  (day if len(day) < 2 else ('0' + day)) +\
+                  (hour if len(hour) < 2 else ('0' + hour))
+
+def checkExists(path):
+    if not os.path.exists(path):
+        print '> cannot find ' + path + ' \n> please make sure your dist path is correct.'
+        sys.exit()
+
+def validatePath(path):
+    print '> validating path ' + path
+    checkExists(path)
+    checkExists(os.path.join(path, 'debug', 'armeabi-v7a/libjingle_peerconnection_so.so'))
+    checkExists(os.path.join(path, 'debug', 'arm64-v8a/libjingle_peerconnection_so.so'))
+    checkExists(os.path.join(path, 'debug', 'x86/libjingle_peerconnection_so.so'))
+    checkExists(os.path.join(path, 'debug', 'libjingle_peerconnection.jar'))
+    checkExists(os.path.join(path, 'release', 'armeabi-v7a/libjingle_peerconnection_so.so'))
+    checkExists(os.path.join(path, 'release', 'arm64-v8a/libjingle_peerconnection_so.so'))
+    checkExists(os.path.join(path, 'release', 'x86/libjingle_peerconnection_so.so'))
+    checkExists(os.path.join(path, 'release', 'libjingle_peerconnection.jar'))
+
+def zipLibs(location):
+    print '> zipping .so files into on .jar file...'
+    #zip .so into one .jar
+    if os.path.exists(os.path.join(location, 'lib')):
+        shutil.rmtree(os.path.join(location, 'lib'))
+    os.makedirs(os.path.join(location, 'lib'))
+    cmd = ['mv', 'armeabi-v7a', 'arm64-v8a', 'x86', 'lib']
+    subprocess.call(cmd, cwd = location)
+    cmd = ['jar', 'cf', 'lib.jar', 'lib']
+    if subprocess.call(cmd, cwd = location):
+        sys.exit()
+
+def publishLibs(location, version):
+    for s in ['debug', 'release']:
+        path = os.path.join(location, s)
+        publish(os.path.join(path, 'libjingle_peerconnection.jar'), 'libjingle-jar-' + s, version)
+        publish(os.path.join(path, 'lib.jar'), 'libjingle-so-' + s, version)
+
+def publish(file_name, artifact_id, version):
+    print '> publishing ' + artifact_id + '...'
+    cmd = 'curl -v -F r=%s -F hasPom=false -F g=%s -F a=%s -F v=%s -F p=%s -F file=@%s -u %s:%s %s' \
+           % (REPOSITORY, GROUP_ID, artifact_id, version, PACKAGING, file_name, USERNAME, PASSWORD, URL)
+    (status, output) = commands.getstatusoutput(cmd)
+    print '\n ==============================================='
+    print output
+    print '\n ===============================================\n'
+    if status != 0:
+        sys.exit()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--dist_location', default = os.path.dirname(__file__), dest = 'dist',
+        help = 'Location of the stack libs to be published.')
+    parser.add_argument('--version', default = DEFAULT_VERSION, dest = 'version',
+        help = 'Version code for the stack libs.')
+
+    options = parser.parse_args()
+    validatePath(options.dist)
+    zipLibs(os.path.join(options.dist, 'debug'))
+    zipLibs(os.path.join(options.dist, 'release'))
+    publishLibs(options.dist, options.version)
