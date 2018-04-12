929c241ec7d4d8f2c0674527f6e1d8ce87dc2abb
Qiu Jianlin <jianlin.qiu@intel.com>
2018-04-12 16:54:32 +0800

    ICFWX-1454: c++ conference client crash when AttachARGBVideoRenderer.
    Change-Id: Idea78c5387e23ffb7bd9281c7ff84f4916eb4e9e
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/122204
    Tested-by: webrtctest <webrtctest@intel.com>
    Reviewed-by: Zhu, Jianjun <jianjun.zhu@intel.com>

--- talk/ics/sdk/base/webrtcvideorendererimpl.cc
+++ talk/ics/sdk/base/webrtcvideorendererimpl.cc
@@ -20,6 +20,10 @@ namespace ics {
 namespace base {
 
 void WebrtcVideoRendererARGBImpl::OnFrame(const webrtc::VideoFrame& frame) {
+  if (frame.video_frame_buffer()->type() ==
+    webrtc::VideoFrameBuffer::Type::kNative) {
+    return;
+  }
   Resolution resolution(frame.width(),frame.height());
   uint8_t* buffer = new uint8_t[resolution.width * resolution.height * 4];
   webrtc::ConvertFromI420(frame, webrtc::VideoType::kARGB, 0, static_cast<uint8_t*>(buffer));
