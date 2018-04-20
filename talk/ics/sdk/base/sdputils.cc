48614c3eb9d7db56ea163b51830c63e75d055640
Qiu Jainlin <jianlin.qiu@intel.com>
2018-04-20 10:04:23 +0800

    Unify sdp util to c++14 implementation as we move linux build to gcc5
    Change-Id: Ie7ad63246fd812af17feb5c3e2d9602b5acfd810
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/124922
    Tested-by: webrtctest <webrtctest@intel.com>
    Reviewed-by: Qiu, Jianlin <jianlin.qiu@intel.com>

--- talk/ics/sdk/base/sdputils.cc
+++ talk/ics/sdk/base/sdputils.cc
@@ -2,11 +2,7 @@
  * Intel License
  */
 
-#if defined(WEBRTC_LINUX)
-#include <regex.h>
-#else
 #include <regex>
-#endif
 #include <sstream>
 #include <vector>
 #include <unordered_map>
@@ -92,85 +88,6 @@ std::vector<std::string> SdpUtils::GetCodecValues(const std::string& sdp,
 std::string SdpUtils::SetPreferCodecs(const std::string& sdp,
     std::vector<std::string>& codec_names,
     bool is_audio) {
-#if defined(WEBRTC_LINUX)
-#define MAX_RTP_MATCHES 10
-    regex_t regex_m_line;
-    regex_t regex_rtp_map;
-    std::string media_type;
-    int status;
-    media_type = is_audio ? "audio" : "video";
-    std::string m_line_reg = "m=" + media_type + ".*\r\n";
-    std::string rtp_line_reg = "a=rtpmap:([[:digit:]]+) " + codec_name + ".*\r\n";
-    int matches = MAX_RTP_MATCHES; //For safety, allow at most 10 matches. Actually we only need 1.
-    regmatch_t m[MAX_RTP_MATCHES];
-    regmatch_t r[MAX_RTP_MATCHES];
-    status = regcomp(&regex_m_line, m_line_reg.c_str(),
-        REG_EXTENDED | REG_NEWLINE | REG_ICASE);
-    if (status != 0) {
-      LOG(LS_INFO) << "Failed to comiple mline regex";
-      regfree(&regex_m_line);
-      return sdp;
-    }
-    status = regcomp(&regex_rtp_map, rtp_line_reg.c_str(),
-        REG_EXTENDED | REG_NEWLINE | REG_ICASE);
-    if (status != 0) {
-      LOG(LS_INFO) << "Failed to comiple rtpmap regex";
-      regfree(&regex_m_line);
-      regfree(&regex_rtp_map);
-      return sdp;
-    }
-    status = regexec(&regex_rtp_map, sdp.c_str(), matches, r, 0);
-    if (status != 0) {
-      LOG(LS_INFO) << "Failed to find match for rtpmap";
-      regfree(&regex_m_line);
-      regfree(&regex_rtp_map);
-      return sdp;
-    }
-
-    std::string codec_value(sdp.substr(r[1].rm_so, r[1].rm_eo - r[1].rm_so));
-    LOG(LS_INFO) << "RTP map found for " << codec_name << ": "
-        << codec_value;
-
-    status = regexec(&regex_m_line, sdp.c_str(), matches, m, 0);
-    if (status != 0) {
-      LOG(LS_INFO) << "Failed to find match for mline";
-      regfree(&regex_rtp_map);
-      regfree(&regex_m_line);
-      return sdp;
-    }
-    std::string m_line(sdp.substr(m[0].rm_so, m[0].rm_eo - m[0].rm_so));
-    std::vector<std::string> m_line_vector;
-    std::stringstream original_m_line_stream(m_line);
-    std::string item;
-    while (std::getline(original_m_line_stream, item, ' ')) {
-        m_line_vector.push_back(item);
-    }
-    if (m_line_vector.size() < 3) {
-      LOG(LS_INFO) << "Wrong SDP format description: " << m_line;
-      regfree(&regex_rtp_map);
-      regfree(&regex_m_line);
-      return sdp;
-    }
-    std::stringstream m_line_stream;
-    for (int i = 0; i < 3; i++) {
-        m_line_stream << m_line_vector[i] << " ";
-    }
-    m_line_stream << codec_value;
-    for (int i = 3, m_line_vector_size = m_line_vector.size();
-      i < m_line_vector_size; i++) {
-      if (m_line_vector[i] != codec_value) {
-          m_line_stream << " " << m_line_vector[i];
-      }
-    }
-
-    std::stringstream new_sdp;
-    new_sdp << sdp.substr(0, m[0].rm_so) << m_line_stream.str()
-        << sdp.substr(m[0].rm_eo, std::string::npos);
-    regfree(&regex_rtp_map);
-    regfree(&regex_m_line);
-    return new_sdp.str();
-
-#else
   // Search all rtx maps in the sdp.
   std::regex reg_fmtp_apt(
       "a=fmtp:(\\d+) apt=(\\d+)(?=[\r]?[\n]?)",
@@ -302,7 +219,6 @@ std::string SdpUtils::SetPreferCodecs(const std::string& sdp,
     }
   }
   return after_strip;
-#endif
 }
 }
 }
