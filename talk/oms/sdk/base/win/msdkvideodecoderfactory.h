536e8be7ba90ecf50b9004719dd6fb8bd1dc9b16
Qiu Jianlin <jianlin.qiu@intel.com>
2018-12-10 12:45:40 +0800

    windows sdk media pipeline reconstruction

--- talk/oms/sdk/base/win/msdkvideodecoderfactory.h
+++ talk/oms/sdk/base/win/msdkvideodecoderfactory.h
@@ -1,17 +1,29 @@
 /*
  * Intel License
  */
+ 
+#ifndef OMS_BASE_WIN_MSDKVIDEODECODERFACTORY_H_
+#define OMS_BASE_WIN_MSDKVIDEODECODERFACTORY_H_
+
 #include "webrtc/media/engine/webrtcvideodecoderfactory.h"
-#pragma once
+
+namespace oms {
+namespace base {
 // Declaration of MSDK based decoder factory.
 class MSDKVideoDecoderFactory
     : public cricket::WebRtcVideoDecoderFactory {
 public:
  MSDKVideoDecoderFactory();
  virtual ~MSDKVideoDecoderFactory();
+
  // WebRtcVideoDecoderFactory implementation.
  webrtc::VideoDecoder* CreateVideoDecoder(webrtc::VideoCodecType type) override;
+
  void DestroyVideoDecoder(webrtc::VideoDecoder* decoder) override;
+
 private:
-    std::vector<webrtc::VideoCodecType> supported_codec_types_;
+ std::vector<webrtc::VideoCodecType> supported_codec_types_;
 };
+}
+}  // namespace oms
+#endif  // OMS_BASE_WIN_MSDKVIDEODECODERFACTORY_H_
