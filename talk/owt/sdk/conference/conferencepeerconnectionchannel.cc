08507f5c16b88e0e93c78512ca4a4663d9c8b3dd
taste1981 <jianlin.qiu@intel.com>
2019-05-07 16:44:26 +0800

    onError is not invoked on pub/sub if client disconnects (#117)
    * Throw OnError event for pub/sub in case there's an ICE failure or server reported connection failure.
    
    * Fire onError event before destroy pub/sub sessions

--- talk/owt/sdk/conference/conferencepeerconnectionchannel.cc
+++ talk/owt/sdk/conference/conferencepeerconnectionchannel.cc
@@ -872,6 +872,10 @@ void ConferencePeerConnectionChannel::OnStreamError(
   std::shared_ptr<const Exception> e(
       new Exception(ExceptionType::kConferenceUnknown, error_message));
   std::shared_ptr<Stream> error_stream;
+  for (auto its = observers_.begin(); its != observers_.end(); ++its) {
+    RTC_LOG(LS_INFO) << "On stream error.";
+    (*its).get().OnStreamError(error_stream, e);
+  }
   if (published_stream_) {
     Unpublish(GetSessionId(), nullptr, nullptr);
     error_stream = published_stream_;
@@ -883,10 +887,6 @@ void ConferencePeerConnectionChannel::OnStreamError(
   if (error_stream == nullptr) {
     RTC_DCHECK(false);
     return;
-  }
-  for (auto its = observers_.begin(); its != observers_.end(); ++its) {
-    RTC_LOG(LS_INFO) << "On stream error.";
-    (*its).get().OnStreamError(error_stream, e);
   }
 }
 std::function<void()> ConferencePeerConnectionChannel::RunInEventQueue(
