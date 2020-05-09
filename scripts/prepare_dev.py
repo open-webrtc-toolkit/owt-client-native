d24fdd478f6f7d92ec46bb281cedc91748bf3fb0
taste1981 <jianlin.qiu@intel.com>
2020-05-09 15:18:54 +0800

    Rebase webrtc to M83 (#320)
    * update deps for M83
    
    * Re-enable hevc and openssl build.
    
    This re-enables HEVC and openssl build. Function not verified. Clang build
    supported only.
    
    * Fix ffmpeg build issue with msvc

--- scripts/prepare_dev.py
+++ scripts/prepare_dev.py
@@ -18,6 +18,7 @@ TESTING_PATH = os.path.join(HOME_PATH, 'testing')
 THIRD_PARTY_PATH = os.path.join(HOME_PATH, 'third_party')
 LIBSRTP_PATH = os.path.join(THIRD_PARTY_PATH, 'libsrtp')
 LIBJPEG_TURBO_PATH = os.path.join(THIRD_PARTY_PATH, 'libjpeg_turbo')
+FFMPEG_PATH = os.path.join(THIRD_PARTY_PATH, 'ffmpeg')
 WEBRTC_OVERRIDES_PATH = os.path.join(THIRD_PARTY_PATH, 'webrtc_overrides')
 BUILD_PATH = os.path.join(HOME_PATH, 'build')
 TOOL_PATH = os.path.join(HOME_PATH, 'tools')
@@ -32,8 +33,6 @@ def _patch():
     subprocess.call(['git', 'am', '--skip'], shell=useShell, cwd=THIRD_PARTY_PATH)
   if (subprocess.call(['git', 'am', os.path.join(PATCH_PATH, '0002-Use-OpenSSL-for-libsrtp.patch')], shell=useShell, cwd=LIBSRTP_PATH)) != 0:
     subprocess.call(['git', 'am', '--skip'], shell=useShell, cwd=LIBSRTP_PATH)
-  #if (subprocess.call(['git', 'am', os.path.join(PATCH_PATH, '0003-Start-iOS-simulator-before-running-tests.patch')], shell=useShell, cwd=TESTING_PATH)) != 0:
-  #  subprocess.call(['git', 'am', '--skip'], shell=useShell, cwd=TESTING_PATH)
   if (subprocess.call(['git', 'am', os.path.join(PATCH_PATH, '0004-Remove-webrtc_overrides.patch')], shell=useShell, cwd=THIRD_PARTY_PATH)) != 0:
     subprocess.call(['git', 'am', '--skip'], shell=useShell, cwd=THIRD_PARTY_PATH)
   if (subprocess.call(['git', 'am', os.path.join(PATCH_PATH, '0005-Fixed-compile-issue-and-disable-thin-archive.patch')], shell=useShell, cwd=BUILD_PATH)) != 0:
@@ -44,16 +43,14 @@ def _patch():
   #  subprocess.call(['git', 'am', '--skip'], shell=useShell, cwd=BUILD_PATH)
   #if (subprocess.call(['git', 'am', os.path.join(PATCH_PATH, '0006-Adjusted-jni_generator.py-to-fit-OWT-code-structure.patch')], shell=useShell, cwd=BASE_PATH)) != 0:
   #  subprocess.call(['git', 'am', '--skip'], shell=useShell, cwd=BASE_PATH)
-  #if (subprocess.call(['git', 'am', os.path.join(PATCH_PATH, '0008-ios-Various-build-fixes-for-Xcode-10.patch')], shell=useShell, cwd=BUILD_PATH)) != 0:
-   # subprocess.call(['git', 'am', '--skip'], shell=useShell, cwd=BUILD_PATH)
   if (subprocess.call(['git', 'am', os.path.join(PATCH_PATH, '0009-Export-WebRTC-symbols-on-iOS.patch')], shell=useShell, cwd=BUILD_PATH)) != 0:
     subprocess.call(['git', 'am', '--skip'], shell=useShell, cwd=BUILD_PATH)
-  #if (subprocess.call(['git', 'am', os.path.join(PATCH_PATH, '0010-Remove-telemetry.patch')], shell=useShell, cwd=TOOL_PATH)) != 0:
-    subprocess.call(['git', 'am', '--skip'], shell=useShell, cwd=TOOL_PATH)
   if (subprocess.call(['git', 'am', os.path.join(PATCH_PATH, '0011-libjpeg_turbo-fix-for-CVE-2018-20330-and-19664.patch')], shell=useShell, cwd=LIBJPEG_TURBO_PATH)) != 0:
     subprocess.call(['git', 'am', '--skip'], shell=useShell, cwd=LIBJPEG_TURBO_PATH)
-  #if (subprocess.call(['git', 'am', os.path.join(PATCH_PATH, '0012-ios-Adds-CFBundleShortVersionString-to-the-plist-for.patch')], shell=useShell, cwd=TESTING_PATH)) != 0:
-    subprocess.call(['git', 'am', '--skip'], shell=useShell, cwd=TESTING_PATH)
+  if (subprocess.call(['git', 'am', os.path.join(PATCH_PATH, '0013-Remove-unused-gni-for-av1-build.patch')], shell=useShell, cwd=THIRD_PARTY_PATH)) != 0:
+    subprocess.call(['git', 'am', '--skip'], shell=useShell, cwd=THIRD_PARTY_PATH)
+  if (subprocess.call(['git', 'am', os.path.join(PATCH_PATH, '0014-Fix-missing-ffmpeg-configure-item-for-msvc-build.patch')], shell=useShell, cwd=FFMPEG_PATH)) != 0:
+    subprocess.call(['git', 'am', '--skip'], shell=useShell, cwd=FFMPEG_PATH)
 
 def main(argv):
   _patch()
