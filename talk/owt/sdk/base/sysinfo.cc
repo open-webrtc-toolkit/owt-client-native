61958dd179a9b07246a1bed84491a4578babe51d
Jianjun Zhu <jianjun.zhu@intel.com>
2020-06-04 11:31:04 +0800

    Bump version number to 4.3.1.

--- talk/owt/sdk/base/sysinfo.cc
+++ talk/owt/sdk/base/sysinfo.cc
@@ -8,7 +8,7 @@
 #include "talk/owt/sdk/base/sysinfo.h"
 namespace owt {
 namespace base {
-static const std::string kSdkVersion("4.3");
+static const std::string kSdkVersion("4.3.1");
 static const std::string kRuntimeName("WebRTC");
 static const std::string kRuntimeVersion("76");
 static const std::string kUnknown("Unknown");
