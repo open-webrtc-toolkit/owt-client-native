689b1718b1a070bf087077d606e5c6b69bd5c733
Qiu Jianlin <jianlin.qiu@intel.com>
2020-06-04 14:13:44 +0800

    Update license headers

--- talk/ics/sdk/base/customizedaudiodevicemodule.h
+++ talk/ics/sdk/base/customizedaudiodevicemodule.h
@@ -2,6 +2,16 @@
  * Intel License
  */
 
+/*
+ * Copyright (c) 2013 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the thirdpartylicense.txt file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
 #ifndef ICS_BASE_CUSTOMIZEDAUDIODEVICEMODULE_H_
 #define ICS_BASE_CUSTOMIZEDAUDIODEVICEMODULE_H_
 
