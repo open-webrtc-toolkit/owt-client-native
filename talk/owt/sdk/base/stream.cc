39ed411efc56bd916b1ddcf75e0ac5cb6bd0859a
ShiJinCheng <874042641@qq.com>
2021-03-10 11:06:20 +0800

    Enable AttachVideoRenderer of VideoRendererInterface on linux. (#470)
    Co-authored-by: JinChengShi <jincheng.shi@zealcomm.cn>

--- talk/owt/sdk/base/stream.cc
+++ talk/owt/sdk/base/stream.cc
@@ -204,7 +204,7 @@ void Stream::AttachAudioPlayer(AudioPlayerInterface& player) {
   RTC_LOG(LS_INFO) << "Attached the stream to a renderer.";
 }
 
-#if defined(WEBRTC_WIN)
+#if defined(WEBRTC_WIN) || defined(WEBRTC_LINUX)
 void Stream::AttachVideoRenderer(VideoRendererInterface& renderer) {
   if (media_stream_ == nullptr) {
     RTC_LOG(LS_ERROR) << "Cannot attach an audio only stream to a renderer.";
