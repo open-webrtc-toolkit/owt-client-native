2b6aec521b7b944fb8e99a1bee42c8a44c97b658
Qiu Jainlin <jianlin.qiu@intel.com>
2021-04-20 16:14:42 +0800

    Add port range setting to pcd factory

--- talk/owt/sdk/base/peerconnectiondependencyfactory.cc
+++ talk/owt/sdk/base/peerconnectiondependencyfactory.cc
@@ -225,6 +225,24 @@ PeerConnectionDependencyFactory::CreatePeerConnectionOnCurrentThread(
   std::unique_ptr<cricket::PortAllocator> port_allocator;
   port_allocator.reset(new cricket::BasicPortAllocator(
       network_manager_.get(), packet_socket_factory_.get()));
+  // Handle port ranges.
+  int audio_min, audio_max, video_min, video_max, screen_min, screen_max,
+      data_min, data_max;
+  GlobalConfiguration::GetPortRanges(audio_min, audio_max, video_min, video_max,
+                                     screen_min, screen_max, data_min,
+                                     data_max);
+  if (audio_min > 0 && audio_max > audio_min) {
+    port_allocator->SetAudioPortRange(audio_min, audio_max);
+  }
+  if (video_min > 0 && video_max > video_min) {
+    port_allocator->SetVideoPortRange(video_min, video_max);
+  }
+  if (screen_min > 0 && screen_max > screen_min) {
+    port_allocator->SetScreenPortRange(screen_min, screen_max);
+  }
+  if (data_min > 0 && data_max > data_min) {
+    port_allocator->SetDataPortRange(data_min, data_max);
+  }
   return (pc_factory_->CreatePeerConnection(config, std::move(port_allocator),
                                             nullptr, observer))
       .get();
