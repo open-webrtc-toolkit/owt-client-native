da2242816294f3224deba4c9d70acbf771d30a50
jianjunz <jianjun.zhu@intel.com>
2023-01-17 10:27:46 +0800

    Update version info. (#665)

--- talk/owt/sdk/base/sysinfo.cc
+++ talk/owt/sdk/base/sysinfo.cc
@@ -12,9 +12,9 @@
 #include "talk/owt/sdk/base/sysinfo.h"
 namespace owt {
 namespace base {
-static const std::string kSdkVersion("5.0");
+static const std::string kSdkVersion("5.1");
 static const std::string kRuntimeName("WebRTC");
-static const std::string kRuntimeVersion("83");
+static const std::string kRuntimeVersion("108");
 static const std::string kUnknown("Unknown");
 std::string SysInfo::SdkType() {
 #if defined(WEBRTC_IOS) || defined(WEBRTC_MAC)
